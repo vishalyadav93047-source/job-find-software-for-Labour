84328b69538045eeaec05bac329181377812f136
vishalyadav93047 <vishalyadav93047@gmail.com>
2026-07-17 22:43:44 +0530

    1st

--- job-find-labour/src/App.jsx
+++ job-find-labour/src/App.jsx
@@ -1,46 +1,39 @@
-import { useState } from 'react'
-import './App.css'
-import Home from './Pages/Home/Home'
-import { Route, Routes } from 'react-router-dom'
-import About from './Pages/About/About'
-import Job from './Components/Jobs/job'
-import Contact from './Pages/Contact/Contact'
-import Mainnav from './Components/Mainnav/Mainnav'
-import Login from './Components/Login/Login'
-import SignUp from './Components/SignUp/SignUp'
-import Jobs from './Pages/Jobs/Jobs'
-import JobFinder from './Components/Jobfinder/JobFinder'
-import Alljobs from './Components/Alljobs/Alljobs'
-import Working from './Components/Workingpage/Working'
-import AlljobsDetails from './Components/AlljobsDetails/AlljobsDetails'
+import './App.css';
+import { Routes, Route } from "react-router-dom";
 
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import Jobs from "./Pages/Jobs/Jobs";
+import Contact from "./Pages/Contact/Contact";
+import Login from "./Components/Login/Login";
+import SignUp from "./Components/SignUp/SignUp";
+import JobFinder from "./Components/Jobfinder/JobFinder";
+import Alljobs from "./Components/Alljobs/Alljobs";
+import Working from "./Components/Workingpage/Working";
+import AlljobsDetails from "./Components/AlljobsDetails/AlljobsDetails";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-       <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/About' element={<About/>}/>
-          <Route path='/Jobs' element={<Jobs/>}/>
-          <Route path='/Contact' element={<Contact/>}/>
-          <Route path='/Login' element={<Login/>}/>
-          <Route path='/SignUp' element={<SignUp/>}/>
-          <Route path='/LogIn' element={<Login/>}/>
-          <Route path='/JobFinder' element={<JobFinder/>}/>
-          <Route path='/Alljobs' element={<Alljobs/>}/>
-          <Route path='/Working' element={<Working/>}/>
-          <Route path='/jobs/:id' element={<AlljobsDetails/>}/>
+    
+    <Routes>
 
-  
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/jobs" element={<Jobs />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/jobfinder" element={<JobFinder />} />
+      <Route path="/alljobs" element={<Alljobs />} />
 
-          
-        </Routes>
+      {/* Dynamic Route */}
+      <Route path="/jobs/:id" element={<AlljobsDetails />} />
 
-        
-    </>
-  )
+      <Route path="/working" element={<Working />} />
+
+    </Routes>
+
+  );
 }
 
-export default App
+export default App;
--- job-find-labour/src/Components/AlljobsDetails/AlljobsDetails.jsx
+++ job-find-labour/src/Components/AlljobsDetails/AlljobsDetails.jsx
@@ -1,88 +1,122 @@
-import React, { useEffect, useState } from 'react'
-import "./AlljobsDetails.css"
-import labour from "../../assets/labour.jpeg"
+import React from "react";
+import "./AlljobsDetails.css";
+import labour from "../../assets/labour.jpeg";
 import { FaLocationDot } from "react-icons/fa6";
 import { useParams } from "react-router-dom";
 
+const jobs = [
+  {
+    id: 1,
+    title: "Electrician",
+    // Name:"Abc",
+    location: "Delhi",
+    salary: "₹500 / Day",
+    type: "Full Time",
+  },
+  {
+    id: 2,
+    title: "Plumber",
+    Name: "Abc",
+    location: "Gurgaon",
+    salary: "₹600 / Day",
+    type: "Full Time",
+  },
+  {
+    id: 3,
+    title: "Barber",
+    Name: "Abc",
+    location: "Noida",
+    salary: "₹300 / Day",
+    type: "Part Time",
+  },
+  {
+    id: 4,
+    title: "Welder",
+    Name: "Abc",
+    location: "Faridabad",
+    salary: "₹700 / Day",
+    type: "Full Time",
+  },
+  {
+    id: 5,
+    title: "Carpenter",
+    Name: "Abc",
+    location: "Jaipur",
+    salary: "₹700 / Day",
+    type: "Full Time",
+  },
+  {
+    id: 6,
+    title: "Painter",
+    Name: "Abc",
+    location: "Lucknow",
+    salary: "₹600 / Day",
+    type: "Full Time",
+  },
+  {
+    id: 7,
+    title: "House Help",
+    Name: "Abc",
+    location: "Lucknow",
+    salary: "₹400 / Day",
+    type: "Full Time",
+  },
+  {
+    id: 8,
+    title: "Technician",
+    Name: "Abc",
+    location: "Lucknow",
+    salary: "₹800 / Day",
+    type: "Full Time",
+  },
+];
 
 function AlljobsDetails() {
+  const { id } = useParams();
 
-    const {id} = useParams();
+  const product = jobs.find((job) => job.id === Number(id));
 
-    const [product, setProduct] = useState(null);
-
-    //  const job = jobs.find((job) => job.id === Number(id));
-
-    useEffect(()=>{
-        fetch(`jobs/${id}`)
-        .then((responce) => responce.json())
-        .then((data) => setProduct(data));
-    }, [id]);
-
-
-    if (!product){
-        return <h1>Loading......</h1>
-    }
-
+  if (!product) {
+    return <h2>Job Not Found</h2>;
+  }
 
   return (
-    <>
-    
-     <div className="AlljobsDetail">
-    
-          <div className="AlljobsDetail1">
+    <div className="AlljobsDetail">
+      <div className="AlljobsDetail1">
+        <div className="AlljobsDetailcard">
 
-            <div className="AlljobsDetaillist">
-    
-              <h2>50+ Jobs</h2>
-    
-              {jobs.map((job) => (
-                <div className="AlljobsDetailcard" key={job.id}>
-    
-                  <div className="AlljobsDetailogo">
-                    <img src={labour} alt="Footer" />
-                  </div>
-    
-                  <div className="AlljobsDetaildetails">
-    
-                    <div className="AlljobsDetail-top-row">
-                      <h3>{job.title}</h3>
-    
-                      <span className="AlljobsDetail-badge">
-                        {job.type}
-                      </span>
-                    </div>
-    
-                    <h4>{job.Name}</h4>
-    
-                    <p>
-                      <FaLocationDot /> {job.location}
-                    </p>
-    
-                    {/* <p>{job.desc}</p> */}
-    
-                  </div>
-    
-                  <div className="AlljobsDetail-job-right">
-    
-                    <h3>{job.salary}</h3>
-    
-                    <button>
-                      View Details 
-                    </button>
-    
-                  </div>
-    
-                </div>
-              ))}
-    
-            </div>
-    
+          <div className="AlljobsDetailogo">
+            <img src={labour} alt="Labour" />
           </div>
-    
+
+          <div className="AlljobsDetaildetails">
+
+            <div className="AlljobsDetail-top-row">
+              <h3>{product.title}</h3>
+
+              <span className="AlljobsDetail-badge">
+                {product.type}
+              </span>
+            </div>
+
+            <h4>{product.Name}</h4>
+
+            <p>
+              <FaLocationDot /> {product.location}
+            </p>
+
+          </div>
+
+          <div className="AlljobsDetail-job-right">
+            <h3>{product.salary}</h3>
+
+            <button>Apply Now</button>
+          </div>
+
         </div>
-    </>
-  )
+      </div>
+    </div>
+  );
 }
 
-export default AlljobsDetails
+export default AlljobsDetails;
