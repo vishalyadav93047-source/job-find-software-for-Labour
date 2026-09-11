cc7b4102b6281f8fb5926012a15e2778cd2f0d7a
vishalyadav93047-source <vishalyadav93047@gmail.com>
2026-09-11 11:18:31 -0700

    3rd

--- job-find-labour/src/Pages/Jobs/Jobs.jsx
+++ job-find-labour/src/Pages/Jobs/Jobs.jsx
@@ -7,6 +7,7 @@ import Chooseus from '../../Components/Chooseus/Chooseus'
 import Scroll from '../../Components/Scroll/Scroll'
 import SearchBar from '../../Components/SearchBar/SearchBar'
 import Job from "../../Components/Jobs/job"
+import jobimage from "../../assets/job.jpeg"
 
 
 function Jobs() {
@@ -14,8 +15,20 @@ function Jobs() {
     <>
    
   <div className='jobshero'> 
+
     <Mainnav/>
+
+<div className="jobimage">
+    <img src={jobimage} alt="imagepro" height="300px" width="100%"/>
+
+    <h1>Find the Right job<br/>for <span>Your Skills </span></h1>
+    <p>Connecting skilled workers with the right opportunities </p>
+</div>
+    
+    <div className="jobsearchp">
     <SearchBar/>
+    </div>
+
     <Job/>
     <Scroll/>
 
